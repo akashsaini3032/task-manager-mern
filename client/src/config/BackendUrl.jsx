82d78aefe2015b0865e7104d9ea2a52c33cc3d13
akashsaini3032 <akashsainira@gmail.com>
2025-06-22 17:13:57 +0530

    front end update

--- client/src/config/BackendUrl.jsx
+++ client/src/config/BackendUrl.jsx
@@ -1,6 +1,10 @@
 
 
 
-const BackEndUrl="http://localhost:8080";
+// const BackEndUrl="http://localhost:8080";
 
+// export default BackEndUrl;
+
+
+const BackEndUrl = import.meta.env.VITE_API_URL;
 export default BackEndUrl;
